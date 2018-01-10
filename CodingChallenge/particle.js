537c8af4287b7f51789e0af6fbd18c4d3d1ab80f
JNGUY5 <31697759+JNGUY5@users.noreply.github.com>
2018-01-09 22:45:25 -0500

    Added the files that I have worked on Using atom.
    The partI have gotten to is creating the circles. I stoped at 15:45 on the vid

---
+++ CodingChallenge/particle.js
@@ -0,0 +1,24 @@
+function Particle(x, y, firework) {
+  this.pos = createVector(x, y);
+  if (firework){
+    this.vel = createVector(0, random(-15,-7));
+  }else{
+    this.vel =  p5.Vector.random2D();
+  }
+
+  this.acc = createVector(0, 0);
+
+  this.applyForce = function(force) {
+    this.acc.add(force);
+  }
+  this.update = function() {
+
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  this.show = function() {
+    point(this.pos.x, this.pos.y);
+  }
+}
